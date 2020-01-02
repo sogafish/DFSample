c07a03c6a54fc5c897f2a89f099e1fa8b9d52096
sogafish <k.sogafish@gmail.com>
2020-01-02 21:16:13 +0900

    :tada:

---
+++ DFSample/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  DFSample
+//
+//  Created by k.soga on 2020/01/02.
+//  Copyright © 2020 k.soga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DFSample
+## SwiftUI
